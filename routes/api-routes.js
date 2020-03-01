5ce34ba4bb9778537fe117210e937e7758ec6e77
Sergio Perez <perezsergio97.sp@gmail.com>
2020-02-29 19:25:59 -0600

    finished routes/cleaned the code

---
+++ routes/api-routes.js
@@ -0,0 +1,44 @@
+module.exports = function(app) {
+    const db = require("../models");
+
+    app.get("/api/workouts", function(req, res){
+        db.Workout.find({})
+        .then(dbWorkout => {
+          res.json(dbWorkout);
+        })
+        .catch(err => {
+          res.json(err);
+        });
+    
+    })
+
+    app.put("/api/workouts/:id", function(req, res){
+        console.log(req.params.id)
+        db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body }, $inc: {totalDuration: req.body.duration} }, { new: true })
+        .then(dbWorkout => {
+          res.json(dbWorkout);
+        })
+        .catch(err => {
+          res.json(err);
+        });
+    })
+
+    app.post("/api/workouts", function(req, res){
+    db.Workout.create({}).then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+    })
+
+    app.get("/api/workouts/range", function(req, res){
+        db.Workout.find({}).sort({day: -1}).limit(7)
+        .then(dbWorkout => {
+          res.json(dbWorkout);
+        })
+        .catch(err => {
+          res.json(err);
+        });
+    })
+}
